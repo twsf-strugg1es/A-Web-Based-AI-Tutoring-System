6c6bf3905e95aea20dc9b899bce4afd162b7470b
Niaz Nafi Rahman <niaznafirahman@gmail.com>
2025-07-20 05:07:11 +0600

    Frontend deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://a-web-based-ai-tutoring-system.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
